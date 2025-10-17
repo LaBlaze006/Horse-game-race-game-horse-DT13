0152a8c86515fb5375f66005d6e9bf2faaf334a6
LaBlaze006 <kcarr@students.mags.school.nz>
2025-10-17 23:02:17 +1300

    Requirements

--- 3d/3d-game/ai_horts.gd
+++ 3d/3d-game/ai_horts.gd
@@ -2,6 +2,8 @@ extends CharacterBody3D
 
 @onready var target: Node3D = $"../map/Navpoint/Target"
 @onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
+@onready var global = get_node("/root/Global")
+
 var can_countdown : bool = true
 var lock : bool = true
 # Called when the node enters the scene tree for the first time.
@@ -11,16 +13,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	
+	# Sets target
 	var rand_pos := target.global_position
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	if not lock:
 		navigation_agent_3d.set_target_position(rand_pos)
 	
-	if Input.is_action_just_pressed("ui_accept") and can_countdown:
+	# Start
+	if global.start:
 		_lock(3.5)
 	
+	# Movement
 	var current_agenet_position = global_position
 	var next_path_position = navigation_agent_3d.get_next_path_position()
 	
@@ -28,6 +32,9 @@ func _physics_process(delta: float) -> void:
 	rotation.y = clamp(rotation.y, 0.0, 0.0)
 	#velocity.y = clamp(velocity.y, 0.0, 0.0)
 	look_at(rand_pos)
+	# Restart
+	if Input.is_action_just_pressed("Restart"):
+		get_tree().reload_current_scene()
 	move_and_slide()
 	
 	
--- 3d/3d-game/scenes/navpoint.gd
+++ 3d/3d-game/scenes/navpoint.gd
@@ -1,8 +1,11 @@
 extends Node3D
 
+@onready var global = get_node("/root/Global")
 
 var lock : bool = true
-
+var can_finish : int = 0
+var time : float = 0.0
+var can_time : bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,13 +14,49 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# Timer
+	if can_time:
+		time += delta
+		# Makes target move
 	if not lock:
 		$AnimationPlayer.play("target_move")
 		
-	if Input.is_action_just_pressed("ui_accept"):
+	# Start
+	if global.start:
 		_lock(3.5)
+
+# Restart
+	if Input.is_action_just_pressed("Restart"):
+		get_tree().reload_current_scene()
+		global.start = false
+		global.end = false
 
 
 func _lock(_time):
 	await get_tree().create_timer(_time).timeout
 	lock = false
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.name == "Horse Indiana Jones":
+		if can_finish == 5:
+			can_time = false
+			$Label.visible = true
+			$Label2.visible = true
+			$Label3.text = str(time)
+			global.end = true
+
+
+func _on_midway_body_entered(body: Node3D) -> void:
+	if body.name == "Horse Indiana Jones":
+		can_finish += 1
+		if can_finish == 1:
+			$"Midway/Checkpoint 1".disabled = true
+		elif can_finish == 2:
+			$"Midway/Checkpoint 2".disabled = true
+		elif can_finish ==3:
+			$"Midway/Checkpoint 3".disabled = true
+		elif can_finish == 4:
+			$"Midway/Checkpoint 4".disabled = true
+		elif can_finish == 5:
+			$"Midway/Checkpoint 5".disabled = true
